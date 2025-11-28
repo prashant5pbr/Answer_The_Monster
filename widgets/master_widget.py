374ad1603d66261366c0588cc9a042fad11b4377
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-11-28 13:06:59 +0530

    Created class that contains parameters common to different widgets.

---
+++ widgets/master_widget.py
@@ -0,0 +1,12 @@
+#Class containing the common parameters for different widgets
+class MasterWidget:
+    #Initialised the different parameters as the instance attributes
+    def __init__(self, master, **kwargs):
+        self.master = master
+        self.bg = kwargs.get("bg", None)
+        self.fg = kwargs.get("fg", None)
+        self.row = kwargs.get("row", None)
+        self.column = kwargs.get("column", None)
+        self.sticky = kwargs.get("sticky", None)
+        self.padx = kwargs.get("padx", None)
+        self.pady = kwargs.get("pady", None)
