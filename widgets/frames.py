586ff4abbd6b1f8376fc0c6572640a2649d2ded8
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-11-28 20:43:29 +0530

    Added attrs borderwidth, highlightthickness and highlightbackground.

--- widgets/frames.py
+++ widgets/frames.py
@@ -3,7 +3,8 @@ from widgets.master_widget import MasterWidget
 
 #Class to create frame widgets
 class FrameWidget(MasterWidget):
-    def __init__(self, master, width = None, height = None, relief = None, columnspan = None, **kwargs):
+    def __init__(self, master, width = None, height = None, relief = None, columnspan = None, borderwidth = None, 
+                 highlightthickness = None, highlightbackground = None, **kwargs):
         #Calling the __init__ method of the parent class (MasterWidget)
         super().__init__(master, **kwargs)
 
@@ -11,7 +12,13 @@ class FrameWidget(MasterWidget):
         self.height = height
         self.relief = relief
         self.columnspan = columnspan
+        self.borderwidth = borderwidth
+        self.highlightthickness = highlightthickness
+        self.highlightbackground = highlightbackground
 
         #Create and locate the frame
-        self.frame = tk.Frame(master = self.master, width = self.width, height = self.height, bg = self.bg, fg = self.fg, relief = self.relief)
+        self.frame = tk.Frame(master = self.master, width = self.width, height = self.height, bg = self.bg, fg = self.fg, 
+                              relief = self.relief, borderwidth = self.borderwidth, highlightthickness = self.highlightthickness, 
+                              highlightbackground = self.highlightbackground)
+        
         self.frame.grid(row = self.row, column = self.column, columnspan = self.columnspan, sticky = self.sticky, padx = self.padx, pady = self.pady)
