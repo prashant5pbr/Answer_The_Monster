20fa99e7c0b30ff9028e79b47e85c41729d5674f
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-11-28 13:20:46 +0530

    Created class to create frames.

---
+++ widgets/frames.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from widgets.master_widget import MasterWidget
+
+#Class to create frame widgets
+class FrameWidget(MasterWidget):
+    def __init__(self, master, width = None, height = None, relief = None, columnspan = None, **kwargs):
+        #Calling the __init__ method of the parent class (MasterWidget)
+        super().__init__(master, **kwargs)
+
+        self.width = width
+        self.height = height
+        self.relief = relief
+        self.columnspan = columnspan
+
+        #Create and locate the frame
+        self.frame = tk.Frame(master = self.master, width = self.width, height = self.height, bg = self.bg, fg = self.fg, relief = self.relief)
+        self.frame.grid(row = self.row, column = self.column, columnspan = self.columnspan, sticky = self.sticky, padx = self.padx, pady = self.pady)
