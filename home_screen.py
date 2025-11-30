b726b16d25974e15526407176bd451997628e306
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-11-30 20:37:00 +0530

    Created a class to create widgets in home screen.

---
+++ home_screen.py
@@ -0,0 +1,55 @@
+from widgets import LabelWidget, FrameWidget
+
+#Class to creat home screen
+class Home:
+    #Initialised the app window and the options of custom in-app menu as the instance attributes
+    def __init__(self, master, new_game_option = None, scores_option = None, exit_option = None):
+        self.root = master
+        self.new_game_option = new_game_option
+        self.scores_option = scores_option
+        self.exit_option = exit_option
+
+    #Method to clear the screen
+    def clear_screen(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+    #Method to create the menu
+    def create_menu(self):
+        #Clear the screen
+        self.clear_screen()
+
+        #Grid layout management for the root
+        for i in range(3):
+            self.root.rowconfigure(i, weight = 1)
+            self.root.columnconfigure(i, weight = 1)
+
+        #Create a frame
+        frame = FrameWidget(self.root, row = 1, column = 1, sticky = "nsew")
+
+        #Grid layout management for the frame
+        for i in range(4):
+            frame.frame.rowconfigure(i, weight = 0)
+
+        frame.frame.columnconfigure(0, weight = 1)
+
+        #Create main menu title
+        LabelWidget(frame.frame, text = "Answer The Monster", font = ("Bradley Hand", 80), fg = "red", 
+            row = 0, column = 0, sticky = "w", padx = (100, 0), pady = 50)
+        
+        #Create labels options for in-app menu which can be clicked
+        self.new_game_option = LabelWidget(frame.frame, text = "New Game", font = ("Academy Engraved LET", 40), cursor = "hand2", 
+                                           fg = "red", row = 1, column = 0, sticky = "w", padx = (375, 0))
+
+        self.scores_option = LabelWidget(frame.frame, text = "Scores", font = ("Academy Engraved LET", 40), cursor = "hand2", 
+                                         fg = "red", row = 2, column = 0, sticky = "w", padx = (375, 0))
+
+        self.exit_option = LabelWidget(frame.frame, text = "Exit", font = ("Academy Engraved LET", 40), cursor = "hand2", 
+                                       fg = "red", row = 3, column = 0, sticky = "w", padx = (375, 0))
+        
+        #Lazy importing the GameSetup Class
+        from options import GameSetup
+
+        #Create GameSetup object and bind the given method to the label
+        new_game_object = GameSetup(self.root)
+        self.new_game_option.label.bind("<Button-1>", lambda event : new_game_object.layout())
