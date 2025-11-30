4d8cd79eb6e10fb7bb29c0fd542b1c1f1da4fe30
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-11-30 20:35:54 +0530

    Refactored by moving all the widgets to home_screen.py

--- answer_the_monster_app.py
+++ answer_the_monster_app.py
@@ -1,34 +1,12 @@
-from widgets import AppWindow, FrameWidget, LabelWidget
+from widgets import AppWindow
+from home_screen import Home
 
 #Create a window object
 root = AppWindow(1100, 660, "Answer The Monster")
 
-#Grid layout management for the root
-for i in range(3):
-    root.root.rowconfigure(i, weight = 1)
-    root.root.columnconfigure(i, weight = 1)
-
-#Create a frame
-frame = FrameWidget(root.root, row = 1, column = 1, sticky = "nsew")
-
-#Grid layout management for the frame
-for i in range(4):
-    frame.frame.rowconfigure(i, weight = 0)
-
-frame.frame.columnconfigure(0, weight = 1)
-
-#Creating labels inside frame
-LabelWidget(frame.frame, text = "Answer The Monster", font = ("Bradley Hand", 80), fg = "red", 
-            row = 0, column = 0, sticky = "w", padx = (100, 0), pady = 50)
-
-new_game_option = LabelWidget(frame.frame, text = "New Game", font = ("Academy Engraved LET", 40), cursor = "hand2", fg = "red", 
-                              row = 1, column = 0, sticky = "w", padx = (375, 0))
-
-scores_option = LabelWidget(frame.frame, text = "Scores", font = ("Academy Engraved LET", 40), cursor = "hand2", fg = "red", 
-                            row = 2, column = 0, sticky = "w", padx = (375, 0))
-
-exit_option = LabelWidget(frame.frame, text = "Exit", font = ("Academy Engraved LET", 40), cursor = "hand2", fg = "red", 
-                          row = 3, column = 0, sticky = "w", padx = (375, 0))
+#Create a Home class object
+home = Home(root.root)
+home.create_menu()              #Create menu
 
 #Setup and run the window
 root.start()
