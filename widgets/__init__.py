e08492608c6d23f01010e9380491da402796170f
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-04 14:16:09 +0530

    Added the classes ButtonWidget, EntryWidget and TextWidget.

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,4 +1,7 @@
 from .app_window import AppWindow
 from .labels import LabelWidget
+from .buttons import ButtonWidget
 from .frames import FrameWidget
 from .canvases import CanvasWidget
+from .entry_fields import EntryWidget
+from .text_fields import TextWidget
