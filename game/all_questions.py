45339cd4d0e6174245adc7e813abfdd5bbdd26df
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-09 22:11:57 +0530

    Created dictionary to store questions and their answers.

---
+++ game/all_questions.py
@@ -0,0 +1,12 @@
+#Dictionary that stores the questions and their answers
+questions_data = {
+    "2 + 2 x 2" : "6", 
+    "2 + 3" : "5",
+    "Sum of angles of a triangle (in degrees)" : "180", 
+    "Sum of angles of a quadrilateral (in degrees)" : "360",
+    "The python method used to display information in console" : "print()",
+    "The python method used to check the type of an object" : "type()",
+    "sin²θ + cos²θ" : "1",
+    "tan45°" : "1",
+    "distance/time" : "speed",
+}
