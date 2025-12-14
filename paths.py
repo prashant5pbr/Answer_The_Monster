4ef356094d1522e7c95a08e22a0014c2046bfb06
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-14 18:48:48 +0530

    Added the path to sample database.

--- paths.py
+++ paths.py
@@ -3,3 +3,6 @@ import os
 #Path to the database
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "database", "quiz_game.db")
+
+#Path to the sample database
+SAMPLE_PATH = os.path.join(BASE_DIR, "database", "sample_quiz_data.db")
