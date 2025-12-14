7e58c00543f304138eed9e877c0e4ab9b1697919
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-15 00:46:43 +0530

    Deleted the files as vertical scrolling is not needed.

--- scroll/key_down_scroll.py
+++
@@ -1,6 +0,0 @@
-#Function to make scrollbar scroll on key press (Scroll Down)
-def v_scroll_down(event, scrollable_object):
-    if not scrollable_object.winfo_exists():
-        return
-    
-    scrollable_object.yview_scroll(1, "units")
--- scroll/key_up_scroll.py
+++
@@ -1,6 +0,0 @@
-#Function to make scrollbar scroll on key press (Scroll Up)
-def v_scroll_up(event, scrollable_object):
-    if not scrollable_object.winfo_exists():
-        return
-    
-    scrollable_object.yview_scroll(-1, "units")
