1dbf9ba8ab65d57b7bd3410a90b00fdb3a2e3c4d
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-15 12:54:02 +0530

    Added the lines to specify DATA_PATH as per script/exe file.

--- paths.py
+++ paths.py
@@ -1,8 +1,14 @@
 import os
+import sys
 
-#Path to the database
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "database", "quiz_game.db")
+#Check if the file is runnning as script or as exceutable file and set the path for database
+if getattr(sys, 'frozen', False):
+    BASE_DIR = sys._MEIPASS
+    DATA_PATH = os.path.join(BASE_DIR, "quiz_game.db")
+
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_PATH = os.path.join(BASE_DIR, "database", "quiz_game.db")
 
 #Path to the logo
 LOGO_PATH = os.path.join(BASE_DIR, "atm_logo.png")
