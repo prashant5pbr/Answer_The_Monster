0b74473da309b1e41c5f16005d031c3764faf18e
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2025-12-15 12:40:47 +0530

    Added the path to logo.

--- paths.py
+++ paths.py
@@ -4,5 +4,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "database", "quiz_game.db")
 
+#Path to the logo
+LOGO_PATH = os.path.join(BASE_DIR, "atm_logo.png")
+
 #Path to the sample database
 SAMPLE_PATH = os.path.join(BASE_DIR, "database", "sample_quiz_data.db")
